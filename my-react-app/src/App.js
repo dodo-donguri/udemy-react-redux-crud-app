67a2e632fd4f8127d24160c5254e48ab86b32d2c
USER <user@USERnoMacBook-Pro.local>
2020-12-22 00:45:55 +0900

    create components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-function App() {
+const App = () => {
   return (
     <>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("I am clicked")}} />
+      <Cat />
     </>
+  )
+}
+
+const Cat = () => {
+  return(
+    <div>
+    Meaow!!
+    </div>
   )
 }
 
