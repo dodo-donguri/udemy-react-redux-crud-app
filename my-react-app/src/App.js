f4f30a78c2ac885e88e64b63df82967b78570a45
USER <user@USERnoMacBook-Pro.local>
2020-12-22 01:23:57 +0900

    counter at usestate

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,32 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React,{ useState } from "react";
 
 const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 1},
-  ]
   return (
     <div>
-      {profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />
-      })}
+      <Counter />
     </div>
-  )
-}
+  );
+};
 
-const User = (props) => {
-  return(
-    <div>
-    Hi, I am {props.name}, and {props.age} years old!
-    </div>
-  )
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    setCount(count - 1)
+  }
+  return (
+    <>
+      <div>counter:{count}</div>
+      <button onClick={increment} >+1</button>
+      <button onClick={decrement} >-1</button>
+    </>
+  );
+};
 
 export default App;
