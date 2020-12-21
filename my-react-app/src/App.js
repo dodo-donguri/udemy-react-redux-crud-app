a08592bdb9ba7364c27aa7500e3121f190dc957f
USER <user@USERnoMacBook-Pro.local>
2020-12-22 00:56:44 +0900

    props of components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName" },
+  ]
   return (
-    <>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />
-      <Cat />
-    </>
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })}
+    </div>
   )
 }
 
-const Cat = () => {
+const User = (props) => {
   return(
     <div>
-    Meaow!!
+    Hi, I am {props.name}, and {props.age} years old!
     </div>
   )
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
