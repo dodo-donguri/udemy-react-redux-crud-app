0cdec6eb1ff626f252bc35587f6e2935489c52b0
USER <user@USERnoMacBook-Pro.local>
2020-12-28 10:46:57 +0900

    implment eventsShow

--- my-react-app/src/components/events_new.js
+++ my-react-app/src/components/events_new.js
@@ -32,7 +32,7 @@ class EventsNew extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div>
@@ -53,7 +53,7 @@ class EventsNew extends Component {
         </div>
 
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
         </div>
       </form>
--- my-react-app/src/reducers/events.js
+++ my-react-app/src/reducers/events.js
@@ -1,17 +1,25 @@
-import _ from "lodash"
-import { READ_EVENTS, DELETE_EVENTS, READ_EVENT } from "../actions"
+import _ from "lodash";
+import {
+  CREATE_EVENT,
+  READ_EVENTS,
+  DELETE_EVENTS,
+  READ_EVENT,
+  UPDATE_EVENT,
+} from "../actions";
 
-export default (events = {}, action ) => {
-  switch(action.type) {
+export default (events = {}, action) => {
+  switch (action.type) {
     case READ_EVENT:
-      const data = action.response.data
-      return { ...events, [data.id]: data }
+    case CREATE_EVENT:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     case READ_EVENTS:
-      return _.mapKeys(action.response.data, "id")
+    case UPDATE_EVENT:
+      return _.mapKeys(action.response.data, "id");
     case DELETE_EVENTS:
-      delete events[action.id]
-      return { ...events }
+      delete events[action.id];
+      return { ...events };
     default:
       return events;
   }
-}
+};
